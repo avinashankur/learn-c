3820c603896a2e863ea28060bc34ab361b084360
avinashankur <avinashhankur@gmail.com>
2023-10-26 22:51:54 +0530

    added assgn 7

---
+++ assignments/7.max-of-three.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int x, y, z, max;
+    printf("Enter the numbers: ");
+    scanf("%d %d %d", &x, &y, &z);
+    if (x > y)
+     max = x;
+    else max = y;
+   
+    if (max > z)
+        printf("The largest value is %d.", max);
+    else
+        printf("The largest value is %d.", z);
+}
