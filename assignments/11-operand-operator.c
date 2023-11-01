5635a0ac1da2eb017d1ff5ea9d61bc402afdf536
avinashankur <avinashhankur@gmail.com>
2023-11-01 22:33:32 +0530

    operand and operator

---
+++ assignments/11-operand-operator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    double operand1, operand2;
+    char operator;
+
+    printf("Enter the first operand: ");
+    scanf("%lf", &operand1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator); 
+
+    printf("Enter the second operand: ");
+    scanf("%lf", &operand2);
+
+    double result;
+
+    switch (operator) {
+        case '+':
+            result = operand1 + operand2;
+            break;
+        case '-':
+            result = operand1 - operand2;
+            break;
+        case '*':
+            result = operand1 * operand2;
+            break;
+        case '/':
+            if (operand2 != 0) {
+                result = operand1 / operand2;
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+                return 1; 
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            return 1;
+    }
+
+    printf("Result: %.2lf\n", result);
+
+    return 0;
+}
