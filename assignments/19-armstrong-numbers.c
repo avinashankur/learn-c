b9775b85e7eef3e16187105007fd9de3e2c0de7e
avinashankur <avinashhankur@gmail.com>
2023-11-07 23:48:37 +0530

    added:assign 19

--- assignments/19-armstrong-numbers.c
+++ assignments/19-armstrong-numbers.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <math.h>
- 
-int toDeci(int binary) {
-    int decimal = 0;
-    int base = 1;
+
+int countDigits(int num) {
+    int count = 0;
+    while (num != 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
 }
 
+int isArmstrong(int num) {
+    int originalNum = num;
+    int numDigits = countDigits(num);
+    int sum = 0;
+
+    while (num != 0) {
+        int digit = num % 10;
+        sum += pow(digit, numDigits);
+        num /= 10;
+    }
+
+    return (sum == originalNum);
+}
+
+int main() {
+    printf("Armstrong numbers between 1 and 100:\n");
+
+    for (int i = 1; i <= 200; i++) {
+        if (isArmstrong(i)) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
